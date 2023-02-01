d2bfac4621f28abea3b118f78f80df166e4c89e7
Tryhardie <realkalin@gmail.com>
2023-02-01 18:10:46 +0200

    tasks-02, 03 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsArray = [];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const liRef = document.createElement("li");
+  liRef.classList.add("item");
+  liRef.textContent = ingredient;
+  ingredientsArray.push(liRef);
+});
+
+ingredientsList.append(...ingredientsArray);
