234c94174b924475c0182dd77a4ac120540abbe9
Tryhardie <realkalin@gmail.com>
2023-02-02 02:54:48 +0200

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const changeColorBtn = document.querySelector(".change-color");
+const spanRef = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", changeColor);
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+
+  spanRef.textContent = randomColor;
+  bodyRef.style.backgroundColor = randomColor;
+}
